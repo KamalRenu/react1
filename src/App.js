66a667d987a364512deb2ba3c27db077815dd77a
Kamal <kamalrenu97@gmail.com>
2021-12-17 08:14:38 +0530

    First commit on react project

---
+++ src/App.js
@@ -0,0 +1,143 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './Card';
+
+function App() {
+  let list = [
+    {
+      plan:"Free",
+      price:0,
+      currency:"$",
+      period:"month",
+      features:[
+        {
+          name:"Single User",
+          enable:true
+        },
+        {
+          name:"5GB Storage",
+          enable:true
+        },
+        {
+          name:"Unlimited Public Projects",
+          enable:true
+        },
+        {
+          name:"Community Access",
+          enable:true
+        },
+        {
+          name:"Unlimited Private Projects",
+          enable:false
+        },
+        {
+          name:"Dedicated Phone Support",
+          enable:false
+        },
+        {
+          name:"Free Subdomain",
+          enable:false
+        },
+        {
+          name:"Monthly Status Reports",
+          enable:false
+        }
+      ]
+    },
+    {
+      plan:"PLUS",
+      price:9,
+      currency:"$",
+      period:"month",
+      features:[
+        {
+          name:"5 User",
+          enable:true,
+          isStrong:true
+        },
+        {
+          name:"50GB Storage",
+          enable:true
+        },
+        {
+          name:"Unlimited Public Projects",
+          enable:true
+        },
+        {
+          name:"Community Access",
+          enable:true
+        },
+        {
+          name:"Unlimited Private Projects",
+          enable:true
+        },
+        {
+          name:"Dedicated Phone Support",
+          enable:true
+        },
+        {
+          name:"Free Subdomain",
+          enable:true
+        },
+        {
+          name:"Monthly Status Reports",
+          enable:false
+        }
+      ]
+    },
+    {
+      plan:"PRO",
+      price:49,
+      currency:"$",
+      period:"month",
+      features:[
+        {
+          name:"Unlimited User",
+          enable:true,
+          isStrong:true
+        },
+        {
+          name:"150GB Storage",
+          enable:true
+        },
+        {
+          name:"Unlimited Public Projects",
+          enable:true
+        },
+        {
+          name:"Community Access",
+          enable:true
+        },
+        {
+          name:"Unlimited Private Projects",
+          enable:true
+        },
+        {
+          name:"Dedicated Phone Support",
+          enable:true
+        },
+        {
+          name:"Unlimited Free Subdomain",
+          enable:true
+        },
+        {
+          name:"Monthly Status Reports",
+          enable:true
+        }
+      ]
+    }
+  ]
+  return (
+    <section class="pricing py-5">
+  <div class="container">
+    <div class="row">
+{
+  list.map((obj)=><Card data={obj}></Card>)
+}
+    </div>
+    </div>
+    </section>
+  );
+}
+
+export default App;
